0f02e6a0294e1a01e6e342b69fb9cedd4956c240
mary_dob@hotmail.com <mary_dob@hotmail.com>
2023-06-29 21:52:37 -0500

    update styles remove scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-active');
+  } else {
+    document.body.classList.remove('page__body--menu-active');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
